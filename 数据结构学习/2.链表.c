716b4e269b9bf9634b594ca77ce65b8c84b02917
Roann-seo <liangxin12366@outlook.com>
2026-08-06 18:25:42 +0800

    📁 重构项目结构，新增单链表应用题（双指针查找倒数第k个节点）

--- 数据结构学习/2.链表.c
+++ 数据结构学习/2.链表.c
@@ -61,6 +61,45 @@ int insertNode(Node*L,int pos,ElemType e){
     p->next=q;
     return 0;
 }
+//在指定位置删除数据
+int deleteNode(Node*L,int pos){
+    Node* p=L;
+    int i=0;
+    while(i<pos-1){
+        p=p->next;
+        i++;
+    }//p是要删除位置的前一个元素
+    if(p->next==NULL){
+        printf("要删除位置错误\n");
+        return 0;
+    }
+    Node* q=p->next;//q是要删除的元素
+    p->next=q->next;
+    //printf("被删除的数据%d\n",q->data);
+    free(q);
+    return 1;
+}
+//获取链表长度
+int listlength(Node*L){
+    Node*p=L;
+    int len=0;
+    while(p->next!=NULL){
+        p=p->next;
+        len++;
+    }
+    return len;
+}
+//释放链表
+void freelist(Node*L){
+    Node* p=L->next;
+    Node*q;
+    while(p->next!=NULL){
+        q=p->next;
+        free(p);
+        p=q;
+    }
+    L->next=NULL;
+}
 int main(){
     Node*list=initlist();
     // insertHead(list,10);
@@ -72,5 +111,10 @@ int main(){
     listNode(list);
     insertNode(list,2,15);
     listNode(list);
+    deleteNode(list,2);
+    listNode(list);
+    printf("释放之前链表长度%d\n",listlength(list));
+    freelist(list);
+    printf("释放之后链表长度%d\n",listlength(list));
     return 1;
 }
---
+++ 数据结构学习/3.单链表应用.c
@@ -0,0 +1,76 @@
+// ### ［2019］（15分）
+// 已知一个带有表头节点的单链表，节点结构为
+
+// | data | link |
+// |------|------|
+
+// 假设该链表只给出了头指针`list`。在不改变链表的前提下，请设计一个尽可能高效的算法，查找链表中倒数第k个位置上的结点（k为正整数）。若查找成功，算法输出该节点的data域的值，并返回1；否则，只返回0。要求：
+// 1）描述算法的基本思想；
+// 2）描述算法的详细实现步骤；
+// 3）根据设计思想和实现步骤，采用程序设计语言描述算法（使用C、C++、或Java语言实现），关键之处请给出简要注释。
+#include<stdio.h>
+#include<stdlib.h>
+//使用双指针找到倒数第k个节点
+typedef int Elemtype;
+typedef struct node{
+    Elemtype data;
+    struct node *next;
+}Node;
+int findNodeFS(Node*L,int k){
+    Node*fast=L->next;
+    Node*slow=L->next;
+    for(int i=0;i<k;i++){
+        fast=fast->next;
+        if(fast==NULL){
+            printf("要查找的位置错误\n");
+            return 0;
+        }
+    }
+    while(fast!=NULL){
+        fast=fast->next;
+        slow=slow->next;
+    }
+    printf("第%d位置的数据是%d\n",k,slow->data);
+    return 1;
+}
+Node*initList(){
+    Node*head=(Node*)malloc(sizeof(Node));
+    head->data=0;
+    head->next=NULL;
+    return head;
+}
+Node* get_Tail(Node*L){
+    Node*p=L;
+    while(p->next!=NULL){
+        p=p->next;
+    }
+    return p;
+}
+Node* insertTail(Node*Tail,Elemtype e){
+    Node*p=(Node*)malloc(sizeof(Node));
+    p->data=e;
+    Tail->next=p;
+    p->next=NULL;
+    return p;
+}
+Node*listNode(Node*L){
+    Node*p=L->next;
+    while(p!=NULL){
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+int main(){
+    Node*list=initList();
+    Node*tail=get_Tail(list);
+    tail=insertTail(tail,10);
+    tail=insertTail(tail,20);
+    tail=insertTail(tail,30);
+    tail=insertTail(tail,40);
+    tail=insertTail(tail,50);
+    tail=insertTail(tail,60);
+    tail=insertTail(tail,70);
+    listNode(list);
+    findNodeFS(list,3);
+}
