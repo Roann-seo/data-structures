05506383a7cb1c129d190cbfa4ddb1f3cf4a3c32
Roann-seo <liangxin12366@outlook.com>
2026-08-16 22:35:13 +0800

    ✨ LeetCode 83：删除排序链表中的重复元素；补充21题递归解法

--- 数据结构学习/leetcode刷题/21合并两个有序链表.c
+++ 数据结构学习/leetcode刷题/21合并两个有序链表.c
@@ -40,4 +40,18 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         tail->next=list1;
     }
     return dummy.next;
+    //跳过虚拟头节点
+    //递归
+    if(list1==NULL){
+        return list2;
+    }if(list2==NULL) {
+        return list1;
+    }//递归结束条件
+    if(list1->val<list2->val){
+        list1->next=mergeTwoLists(list1->next,list2);
+        return list1;
+    }else{
+        list2->next=mergeTwoLists(list1,list2->next);
+        return list2;
+    }
 }
---
+++ 数据结构学习/leetcode刷题/83.删除链表中的重复元素.c
@@ -0,0 +1,44 @@
+// 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+// 示例 1：
+
+
+// 输入：head = [1,1,2]
+// 输出：[1,2]
+// 示例 2：
+
+
+// 输入：head = [1,1,2,3,3]
+// 输出：[1,2,3]
+
+
+// 提示：
+
+// 链表中节点数目在范围 [0, 300] 内
+// -100 <= Node.val <= 100
+// 题目数据保证链表已经按升序 排列
+#include <stddef.h>
+    struct ListNode {
+    int val;
+    struct ListNode *next;
+    };
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if(head==NULL){
+        return NULL;
+    }
+    struct ListNode*p=head;
+    struct ListNode*q=head->next;
+    while(q!=NULL){
+        if(p->val==q->val){
+            q=q->next;
+            p->next=q;
+            //p->next=q->next;错误
+            //找到重复节点时，当前指针 p 不要立刻后移！
+            //继续和新的后继节点对比，防止连续重复删不干净。
+        }
+        else{
+            p=q;
+            q=q->next;
+        }
+    }
+    return head;
+}
